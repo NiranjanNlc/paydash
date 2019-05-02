fad92a5412ac4bcf9e3a3fb7d6c1f5d8b9d9381e
ernlc <niranjanlamichhane4@gmail.com>
2019-05-02 06:39:17 -0400

    changes

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -8,13 +8,13 @@ public class test {
 
     public static void main(String[] args) {
         Campagain c = new Campagain();
-//        c.createCampagain("Hello ");
-//        c.createCampagain( "RTI Campagain");
-//        c.createCampagain("Nepal bandai xa");
-//        c.getCampagainObject();
-//        c.campagainIterator();
-//        c.update();;
-//        c.campagainIterator();
+        c.createCampagain("Hello ");
+        c.createCampagain( "RTI Campagain");
+        c.createCampagain("Nepal bandai xa");
+        c.getCampagainObject();
+        c.campagainIterator();
+        c.update();;
+        c.campagainIterator();
             c.getEdgecampagain();
          //   c.getCampagainObject();
     }
